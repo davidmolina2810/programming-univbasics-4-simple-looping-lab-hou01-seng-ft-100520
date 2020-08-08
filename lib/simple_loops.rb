5aef3707f91fbde7af9be912ffd862946b6832f1
David Molina <davidmolina2810@gmail.com>
2020-08-08 22:42:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,12 @@ def loop_message_five_times(message)
     puts message
   end
 end
+
+#1. Get message argument
+#2. Get number of times to loop, n
+#3. puts message n amount of times
+def loop_message_n_times(message, n)
+  n.times do
+    puts message
+  end
+end
